c0040df15539645f7b99a6aa6ed5baadc734ef67
luanfonsecap <luanfonsecap@icloud.com>
2019-10-01 16:02:30 -0300

    atualizado componente de estilo input

--- App/MercadoVerde/src/Components/Input.js
+++ App/MercadoVerde/src/Components/Input.js
@@ -9,11 +9,14 @@ const styles = StyleSheet.create({
     borderColor: '#707070',
     borderWidth: 1,
     elevation: 1,
-    width: 250
+    width: 250,
+    height: 35,
+    padding: 0,
+    paddingLeft: 5
   }
 });
 
 /* Componente de exportação de estilo padrão a ser utilizado em componentes
 do tipo InputText */
 
-export default styles;
+export default styles.input;
