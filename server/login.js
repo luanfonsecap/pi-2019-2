53a445a25b361e5f7506e95a4a452b781e1d5109
Lucas Lima <54602632+hellolusca@users.noreply.github.com>
2019-10-04 15:30:03 -0300

    Add files via upload

---
+++ server/login.js
@@ -0,0 +1,49 @@
+'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 1337 //porta padrão
+const mysql = require('mysql');
+const cors = require('cors')
+
+//configurando o body parser para pegar POSTS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+app.post('/login', (req, res) => {
+    var user = req.body.usuario;
+    var pass = req.body.senha;
+    console.log(user, pass);
+    execSQLQuery(`SELECT nome,tipo FROM cadastro WHERE usuario = "${user}" AND senha = "${pass}"`, res);
+    console.log(res);
+})
+
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res) {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'mercado@verde',
+        database: 'mercadoverde'
+    });
+
+    connection.query(sqlQry, function (error, results, fields) {
+        if (error)
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('OK');
+    });
+}
