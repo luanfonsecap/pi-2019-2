d5d30598f1c4c11c95fe93b038bdb52c376c0bb0
Alexander Delfim <49282595+alexanderdelfim@users.noreply.github.com>
2019-11-01 13:13:42 -0300

    corrigindo modificação

--- server/routes/read.js
+++ server/routes/read.js
@@ -213,7 +213,7 @@ function infoProdutosDestaque(req, res) {
 
 function infoMelhores(req, res) {
     const { cidade } = req.body;
-    const sqlQry = `SELECT id,nome,avaliacao_med_pro,urlImagem FROM cadastro WHERE avaliacao_med >= 4 AND cidade='${cidade}'`;
+    const sqlQry = `SELECT id,nome,avaliacao_med,urlImagem FROM cadastro WHERE avaliacao_med >= 4 AND cidade='${cidade}'`;
 
     connection.query(sqlQry, function (error, results, fields) {
         if (error) {
